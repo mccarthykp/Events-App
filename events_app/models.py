1b58006ae70c9470536f445b0692aea8d33ae4da
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2022-02-02 22:30:13 -0500

    updated comments

--- events_app/models.py
+++ events_app/models.py
@@ -1,15 +1,7 @@
-"""Create database models to represent tables."""
 from datetime import datetime
 from events_app import db
 from sqlalchemy.orm import backref
 import enum
-
-# TODO: Create a model called `Guest` with the following fields:
-# - id: primary key
-# - name: String column
-# - email: String column
-# - phone: String column
-# - events_attending: relationship to "Event" table with a secondary table
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,16 +9,6 @@ class Guest(db.Model):
     email = db.Column(db.String(80), nullable=False)
     phone = db.Column(db.String(80), nullable=False)
     events_attending = db.relationship('Event', secondary='event_guest', back_populates='guests')
-
-# TODO: Create a model called `Event` with the following fields:
-# - id: primary key
-# - title: String column
-# - description: String column
-# - date_and_time: DateTime column
-# - guests: relationship to "Guest" table with a secondary table
-
-# STRETCH CHALLENGE: Add a field `event_type` as an Enum column that denotes the
-# type of event (Party, Study, Networking, etc)
 
 class Event_type(enum.Enum):
     Party = 1
@@ -45,10 +27,6 @@ class Event(db.Model):
         self.title = title
         self.description = description
         self.date_and_time = date_and_time
-
-# TODO: Create a table `guest_event_table` with the following columns:
-# - event_id: Integer column (foreign key)
-# - guest_id: Integer column (foreign key)
 
 guest_event_table = db.Table('event_guest',
     db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
--- events_app/routes.py
+++ events_app/routes.py
@@ -1,4 +1,3 @@
-"""Import packages and modules."""
 import os
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
@@ -7,7 +6,6 @@ from events_app.models import Event, Guest, guest_event_table
 from events_app import app, db
 
 main = Blueprint('main', __name__)
-
 
 ##########################################
 #           Routes                       #
@@ -19,7 +17,6 @@ def index():
     events = Event.query.all()
     return render_template('index.html', events=events)
 
-
 @main.route('/create', methods=['GET', 'POST'])
 def create():
     """Create a new event."""
@@ -30,7 +27,6 @@ def create():
         time = request.form.get('time')
 
         try:
-            # refactor this later
             date_and_time = datetime.strptime(
                 f'{date} {time}',
                 '%Y-%m-%d %H:%M')
@@ -47,21 +43,17 @@ def create():
     else:
         return render_template('create.html')
 
-
 @main.route('/event/<int:event_id>', methods=['GET'])
 def event_detail(event_id):
     """Show a single event."""
 
-    # TODO: Get the event with the given id and send to the template
     event = Event.query.get_or_404(event_id)
     guests = db.session.query(guest_event_table).filter_by(event_id=event.id)
     return render_template('event_detail.html', title=event.title, event=event, guests=guests)
 
-
 @main.route('/event/<event_id>', methods=['POST'])
 def rsvp(event_id):
     """RSVP to an event."""
-    # TODO: Get the event with the given id from the database
     is_returning_guest = request.form.get('returning')
     guest_name = request.form.get('guest_name')
     event = Event.query.get_or_404(event_id)
@@ -69,24 +61,17 @@ def rsvp(event_id):
     if is_returning_guest:
         exists = db.session.query(Guest.id).filter_by(name=guest_name).first() is not None
 
-        # TODO: If the guest does exist, add the event to their 
-        # events_attending, then commit to the database.
         if exists:
             update_guest = Guest.query.filter_by(name=guest_name).one()
             update_guest.events_attending.append(event)
             db.session.add(update_guest)
             db.session.commit()
-        # TODO: Look up the guest by name. If the guest doesn't exist in the 
-        # database, render the event_detail.html template, and pass in an error
-        # message as `error`.
         else:
             return render_template('event_detail.html', error='error', title=event.title, event=event)
     else:
         guest_email = request.form.get('email')
         guest_phone = request.form.get('phone')
 
-        # TODO: Create a new guest with the given name, email, and phone, and 
-        # add the event to their events_attending, then commit to the database.
         new_guest = Guest(name=guest_name, email=guest_email, phone=guest_phone)
         new_guest.events_attending.append(event)
 
@@ -96,11 +81,7 @@ def rsvp(event_id):
     flash('You have successfully RSVP\'d! See you there!')
     return redirect(url_for('main.event_detail', event_id=event_id))
 
-
 @main.route('/guest/<guest_id>')
 def guest_detail(guest_id):
-    # TODO: Get the guest with the given id and send to the template
     guest = Guest.query.get_or_404(guest_id)
     return render_template('guest_detail.html', guest=guest)
-
-# kevin, kevin@du.com, 111 111 1111
